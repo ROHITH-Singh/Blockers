e3cfbe2f3889b6f35282f18c7e91d9773a7b8a3d
ROHITH-Singh <rs9113848@gmail.com>
2021-05-14 01:52:55 -0700

    first stage

---
+++ Blockers/Blockers/VC/home.swift
@@ -0,0 +1,8 @@
+//
+//  Home.swift
+//  Blockers
+//
+//  Created by Rohit on 14/05/21.
+//
+
+import Foundation
--- Blockers/Blockers/ViewController.swift
+++ Blockers/Blockers/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var timer: Timer?
+    @IBOutlet weak var LogoImage: UIImageView!
+    
+    @IBOutlet weak var titleImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        LogoImage.image = UIImage(named: "logo2")
+        titleImage.image = UIImage(named: "title")
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self , selector: #selector(segueMethod),userInfo: nil,repeats: false)
+//        LogoImage.frame.size.height = 600
+//        LogoImage.frame.size.width = 300
+//
         // Do any additional setup after loading the view.
+        
     }
-
+    
+    @objc func segueMethod(){
+        performSegue(withIdentifier: "start", sender: self)
+    }
+  
 
 }
 
